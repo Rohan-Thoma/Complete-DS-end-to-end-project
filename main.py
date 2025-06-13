220a0572da3069033bc9bb87230ba4b89dc15a17
Rohan-Thoma <rohanvailalathoma@gmail.com>
2025-06-13 13:38:43 +0530

    Created data ingestion pipeline and updated in main.py also

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 from src.datascience_project import logger
+from src.datascience_project.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 
-logger.info("Welcome to our custom logging data science")
+STAGE_NAME = 'Data Ingestion Stage'
+
+try:
+        logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.initiate_data_ingestion()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<< \n\n X==========================X")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
 
