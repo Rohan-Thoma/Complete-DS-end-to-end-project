2b192f6ba5d783e31adaabf2ed97fc093b345c2f
Rohan-Thoma <rohanvailalathoma@gmail.com>
2025-06-13 17:10:05 +0530

    Made changes to data_tranformation_pipeline by adding a try catch check for validation status

--- src/datascience_project/pipeline/data_transformation_pipeline.py
+++ src/datascience_project/pipeline/data_transformation_pipeline.py
@@ -4,6 +4,7 @@ from src.datascience_project import logger
 from sklearn.model_selection import train_test_split
 import pandas as pd
 from src.datascience_project.entity.config_entity import DataTransformationConfig
+from pathlib import Path
 
 STAGE_NAME = 'Data Transformation Stage'
 
@@ -12,10 +13,18 @@ class DataTransformationTrainingPipeline:
         pass
 
     def initiate_data_transformation(self):
-        config=ConfigurationManager()
-        data_transformation_config=config.get_data_transformation_config()
-        data_transformation=DataTransformation(config=data_transformation_config)
-        data_transformation.train_test_splitting()
+        try:
+            with open(Path("artifacts/data_validation/status.txt"), 'r') as f:
+                status=f.read().split(" ")[-1]
+            if status == "True":
+                config=ConfigurationManager()
+                data_transformation_config=config.get_data_transformation_config()
+                data_transformation=DataTransformation(config=data_transformation_config)
+                data_transformation.train_test_splitting()
+            else:
+                raise Exception("Your data schema is not valid")
+        except Exception as e:
+            raise e
 
 if __name__=="__main__":
     try:
