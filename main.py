a6f2aa24bf500983c4ebfef046078f4f6986a293
Rohan-Thoma <rohanvailalathoma@gmail.com>
2025-06-14 19:33:04 +0530

    model trainer pipeline is completed

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from src.datascience_project import logger
 from src.datascience_project.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.datascience_project.pipeline.data_validation_pipeline import DataValidationTrainingPipeline 
-from src.datascience_project.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline # type: ignore
-
+from src.datascience_project.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline 
+from src.datascience_project.pipeline.model_trainer_pipeline import ModelTrainerPipeline
 STAGE_NAME = 'Data Ingestion Stage'
 
 try:
@@ -35,3 +35,14 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
+STAGE_NAME= "Model Trainer Stage"
+
+try: 
+        logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = ModelTrainerPipeline()
+        obj.initiate_model_training()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<< \n\n X===============================X")
+except Exception as e:
+        logger.exception(e)
+        raise e   
